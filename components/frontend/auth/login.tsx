f62ebbaab23916b3b5e70af07df1f9d614f35012
a10group <a10group.tech@gmail.com>
2026-02-08 21:45:39 +0000

    Login Form UI finish

--- components/frontend/auth/login.tsx
+++ components/frontend/auth/login.tsx
@@ -29,9 +29,9 @@ export default function Login() {
   }
   return (
     <div className="w-full lg:grid h-screen lg:min-h-[600px] lg:grid-cols-2 relative ">
-      <div className="flex h-screen items-center justify-center py-12">
+      <div className="flex items-center justify-center py-12 mt-10 md:mt-0">
         <div className="mx-auto grid w-[350px] gap-6">
-          <div className="absolute left-1/3 top-15 md:top-5 md:left-5">
+          <div className="absolute left-1/3 top-7 md:top-5 md:left-5">
             <Logo />
           </div>
           <div className="grid gap-2 text-center">
